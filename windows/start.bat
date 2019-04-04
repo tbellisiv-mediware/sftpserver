a4e655e681a3b349de381177bd924a6507924350
Ellis Tellis <tellis.ellis@mediware.com>
2019-04-03 19:06:16 -0500

    Initial commit - WIP

---
+++ windows/start.bat
@@ -0,0 +1,13 @@
+
+echo off
+
+set ID=default
+set SFTPD_HOME=C:/dev/sftp/sftpserver/home
+set SFTPD_MEM_MB=64
+set SFTPD_OPTS=-verbose:gc -XX:+PrintGCDetails -XX:+PrintGCDateStamps -XX:+PrintGCTimeStamps -showversion -XX:+PrintCommandLineFlags -XX:-PrintFlagsFinal
+set SFTPD_POLICY=%SFTPD_HOME%/conf/sftpd.policy
+set MAIN_CLASS=org.javastack.sftpserver.Server
+set JAVA_CMD=java -Dprogram.name=sftpd %SFTPD_OPTS% -Xmx%SFTPD_MEM_MB%m -Dsftp.id=%ID% -Dsftp.home=%SFTPD_HOME% -cp %SFTPD_HOME%/conf/%ID%/;%SFTPD_HOME%/conf/;%SFTPD_HOME%/lib;%SFTPD_HOME%/lib/* -Djava.security.manager -Djava.security.policy=file:%SFTPD_POLICY% %MAIN_CLASS%
+
+echo %JAVA_CMD%
+%JAVA_CMD%
